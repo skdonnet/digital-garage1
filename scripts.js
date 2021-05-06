b43b9b012ea7e993d3318cfd7279d8b9228c2edd
skdonnet <donovan.ruebusch@student.swic.edu>
2021-05-06 18:56:03 -0500

    importing an external array from data, pushtoarray

--- scripts.js
+++ scripts.js
@@ -4,10 +4,22 @@
 // TODO: delete buttons to delete a vehicle from the array?
 // TODO: random vehicle button that choose a random vehicle to take out for the day
 
+import { vehicles } from './data.js';
+
 const form = document.querySelector('#form');
 const submit = document.querySelector('#subButton');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  console.log(event.target.element[0].value);
+
+  vehicles.push({
+    type: event.target.elements[0].value,
+    make: event.target.elements[1].value,
+    model: event.target.elements[2].value,
+    year: event.target.elements[3].value,
+    color: event.target.elements[4].value,
+    horsepower: event.target.elements[5].value,
+    torque: event.target.elements[6].value,
+    weight: event.target.elements[7].value,
+  });
 });
