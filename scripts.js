4f80bbb6737e99e6fab81888d81adfe119d2fa54
skdonnet <donovan.ruebusch@student.swic.edu>
2021-05-06 14:47:04 -0500

    npm install and todo notes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
-
+// TODO: have the user enter in vehicles to an array stored in data
+// TODO: have a submit button that pushes the vehicle into the array
+// TODO: display the vehicles into a listbox of sorts
+// TODO: delete buttons to delete a vehicle from the array?
+// TODO: random vehicle button that choose a random vehicle to take out for the day
