5d1aa2dd216ceaca3f68c6352a3e8629e37cb632
skdonnet <donovan.ruebusch@student.swic.edu>
2021-05-16 14:59:53 -0500

    Starting data

--- data.js
+++ data.js
@@ -1,1 +1,31 @@
-export const vehicles = [];
+// eslint-disable-next-line import/prefer-default-export
+export const vehicles = [
+  {
+    type: 'Car',
+    make: 'Mazda',
+    model: '3',
+    year: '2016',
+    color: 'Grey',
+  },
+  {
+    type: 'Motorcycle',
+    make: 'Kawasaki',
+    model: 'ZX6R',
+    year: '2021',
+    color: 'Green',
+  },
+  {
+    type: 'Motorcycle',
+    make: 'Kawasaki',
+    model: 'KLX 230',
+    year: '2021',
+    color: 'Green',
+  },
+  {
+    type: 'Car',
+    make: 'Nissan',
+    model: '370z',
+    year: '2020',
+    color: 'Silver',
+  },
+];
